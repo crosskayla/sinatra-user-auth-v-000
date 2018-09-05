1d37580b67fdab3c3f24e3bd0a3981fc4c10c47a
Kayla Cross <kac288@georgetown.edu>
2018-09-05 01:10:03 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,8 +16,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/registrations' do
-    @user = User.new(name: params["name"], email: params["email"], password: params["password"])
-    @user.save
+    @user = User.create(name: params["name"], email: params["email"], password: params["password"])
     session[:id] = @user.id
     redirect '/users/home'
   end
@@ -37,7 +36,7 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/users/home' do
-
+    @user = User.find(session[:id])
     erb :'/users/home'
   end
 
